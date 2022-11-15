13f3d9a2e72aaa4297d71298d9cac2a065673efe
OvercomingJunk <overcomingjunk@yandex.ru>
2022-11-15 14:48:06 +0300

    Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,31 +5,52 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Класс описывает модель программы для загрузки файлов в интернете с ограничением скорости
+ * В параметрах запуска указываются - URL страницы в интернете, максимальная скорость скачивания файла
+ * и путь к файлу для записи данных
+ *
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 14.11.22
+ */
 public class Wget implements Runnable {
+    /**
+     * Поле URL страницы в интернете
+     */
     private final String url;
+    /**
+     * Поле максимальная скорость скачивания
+     */
     private final int speed;
+    /**
+     * Поле путь к файлу для записи данных
+     */
+    private final String filePath;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String filePath) {
         this.url = url;
         this.speed = speed;
+        this.filePath = filePath;
     }
 
+    /**
+     * Метод используется для загрузки данных с ограничением скорости скачивания
+     */
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long bytesWrite = 0;
             long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 bytesWrite += bytesRead;
-                System.out.print("\r Loading... " + bytesWrite + " bytes");
                 if (bytesWrite >= speed) {
                     long deltaTime = System.currentTimeMillis() - start;
-                    System.out.println(deltaTime);
                     if (deltaTime < 1000) {
-                        Thread.sleep(10000 - deltaTime);
+                        Thread.sleep(1000 - deltaTime);
                     }
                     bytesWrite = 0;
                     start = System.currentTimeMillis();
@@ -41,6 +62,11 @@ public class Wget implements Runnable {
         }
     }
 
+    /**
+     * Метод используется для валидации входных параметров запуска
+     *
+     * @param args - входные параметры запуска
+     */
     public static void validateArgs(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Укажите в парметрах запуска URL и ограничение скорости!");
@@ -51,13 +77,14 @@ public class Wget implements Runnable {
         if (Integer.parseInt(args[1]) <= 0) {
             throw new IllegalArgumentException("Укажите корректное ограничение скорости в байтах в секунду!");
         }
+        if (!args[2].matches(".+\\..+")) {
+            throw new IllegalArgumentException("Укажите корректный файл для записи данных!");
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         validateArgs(args);
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread get = new Thread(new Wget(url, speed));
+        Thread get = new Thread(new Wget(args[0], Integer.parseInt(args[1]), args[2]));
         get.start();
         get.join();
     }
