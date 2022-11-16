ff3b76b0775dc484dad4fc93ed0947fd8c554e1a
OvercomingJunk <overcomingjunk@yandex.ru>
2022-11-16 22:16:15 +0300

    Модель памяти Java [#267917]

--- src/main/java/ru/job4j/thread/DCLSingleton.java
+++ src/main/java/ru/job4j/thread/DCLSingleton.java
@@ -1,6 +1,8 @@
 package ru.job4j.thread;
 
 /**
+ * Double-Checked Locking with Singleton
+ *
  * @author Artem Chernikov
  * @version 1.0
  * @since 16.11.2022
@@ -10,9 +12,11 @@ public class DCLSingleton {
 
     public static DCLSingleton instOf() {
         if (inst == null) {
+            synchronized (DCLSingleton.class) {
                 if (inst == null) {
                     inst = new DCLSingleton();
                 }
+            }
         }
         return inst;
     }
