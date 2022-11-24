258efc2f20eba23085d002e630071d16d0637599
OvercomingJunk <overcomingjunk@yandex.ru>
2022-11-24 14:43:33 +0300

    Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.ref;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 24.11.2022
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
