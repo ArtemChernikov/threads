800b83faad8666824ebbfc5d7a5018694691ac00
OvercomingJunk <overcomingjunk@yandex.ru>
2022-12-05 14:56:29 +0300

    Обеспечить остановку потребителя

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.buffer;
+
+import ru.job4j.wait.SimpleBlockingQueue;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 05.12.2022
+ */
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).start();
+    }
+}
