c6ca620211e03aae2eed9c2272a3b1cbce01d63e
OvercomingJunk <overcomingjunk@yandex.ru>
2022-12-10 21:11:56 +0300

    Создал рассылку почты с помощью ExecutorService

---
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -0,0 +1,29 @@
+package ru.job4j.mail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 10.12.2022
+ */
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        String username = user.getUsername();
+        String email = user.getEmail();
+        String subject = String.format("Notification {%s} to email {%s}", username, email);
+        String body = String.format("Add a new event to {%s}", username);
+        pool.submit(() -> send(subject, body, email));
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+}
