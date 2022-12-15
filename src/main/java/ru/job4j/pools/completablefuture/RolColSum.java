c4a0631ab266939397db48a5ff988c9b3be01aaf
OvercomingJunk <overcomingjunk@yandex.ru>
2022-12-15 19:50:46 +0300

    add class RolColSum.java

---
+++ src/main/java/ru/job4j/pools/completablefuture/RolColSum.java
@@ -0,0 +1,22 @@
+package ru.job4j.pools.completablefuture;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 15.12.2022
+ */
+public class RolColSum {
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+        /* Getter and Setter */
+    }
+
+    public static Sums[] sum(int[][] matrix) {
+
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) {
+
+    }
+}
