96c1d50952d7f41272b6008bc22963f2e046c572
OvercomingJunk <overcomingjunk@yandex.ru>
2022-12-16 22:52:23 +0300

    Changed test in RolColSum.java

--- src/test/java/ru/job4j/pools/completablefuture/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/completablefuture/RolColSumTest.java
@@ -8,7 +8,7 @@ import static ru.job4j.pools.completablefuture.RolColSum.*;
 class RolColSumTest {
 
     @Test
-    void whenSum() {
+    void whenSum() throws InterruptedException {
         int[][] matrix = {{1, 14, 10}, {1, 22, 33}, {11, 2, 3}};
         Sums[] sums = sum(matrix);
         Sums[] expected = {
@@ -16,11 +16,12 @@ class RolColSumTest {
                 new Sums(56, 38),
                 new Sums(16, 46)
         };
+        Thread.sleep(2000);
         assertThat(sums).isEqualTo(expected);
     }
 
     @Test
-    public void whenAsyncSum() {
+    public void whenAsyncSum() throws InterruptedException {
         int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         Sums[] sums = asyncSum(array);
         Sums[] expected = new Sums[]{
@@ -28,6 +29,7 @@ class RolColSumTest {
                 new Sums(15, 15),
                 new Sums(24, 18)
         };
+        Thread.sleep(2000);
         assertThat(sums).isEqualTo(expected);
     }
 }
